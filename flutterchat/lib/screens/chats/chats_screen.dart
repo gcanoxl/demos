269fea20cd1e2b2bb54a8e59cd5c9a142ed3e779
Cano XL <gcanoxl@gmail.com>
2023-10-21 04:17:58 +0800

    feat: add chats_screen.dart

--- flutterchat/lib/screens/chats/chats_screen.dart
+++ flutterchat/lib/screens/chats/chats_screen.dart
@@ -2,14 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:flutterchat/constants.dart';
 import 'package:flutterchat/screens/chats/components/body.dart';
 
-class ChatsScreen extends StatelessWidget {
+class ChatsScreen extends StatefulWidget {
   const ChatsScreen({super.key});
 
+  @override
+  State<ChatsScreen> createState() => _ChatsScreenState();
+}
+
+class _ChatsScreenState extends State<ChatsScreen> {
+  int _selected = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: buildAppBar(),
-      body: Body(),
+      body: const Body(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: kPrimaryColor,
+        child: const Icon(
+          Icons.person_add_alt_1,
+          color: Colors.white,
+        ),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: _selected,
+      onTap: (value) {
+        setState(() {
+          _selected = value;
+        });
+      },
+      items: const [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.messenger),
+          label: "Messages",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.people),
+          label: "Contact",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.call),
+          label: "Calls",
+        ),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.person),
+          label: "Profile",
+        ),
+      ],
     );
   }
 
